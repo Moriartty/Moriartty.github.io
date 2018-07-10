a4d41b33607c4aabcd7e30b6015cc9b857c5e786
bing-chen <bing-chen@tcl.com>
2018-07-10 16:55:30 +0800

    v0.0.1

---
+++ js/moriarty.js
@@ -0,0 +1,10 @@
+(function(){
+	// function hashChanged(hashObj){
+	// 	let oldHash = hashObj.oldURL.split('#')[1];
+	// 	let newHash = hashObj.newURL.split('#')[1];
+	// 	$('#'+newHash).show().siblings('section').hide();
+	// }
+	// window.onhashchange = hashChanged;
+
+	// console.log($(window).width())
+})()
